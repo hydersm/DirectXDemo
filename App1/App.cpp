c60039051761d38e0f6e5613b89e5ad945d463e4
Hyder SM <hyder.mashrour@autodesk.com>
2015-01-07 11:20:05 -0500

    adding files

---
+++ App1/App.cpp
@@ -0,0 +1,89 @@
+// Include the precompiled headers
+#include "pch.h"
+
+// Use some common namespaces to simplify the code
+using namespace Windows::ApplicationModel;
+using namespace Windows::ApplicationModel::Core;
+using namespace Windows::ApplicationModel::Activation;
+using namespace Windows::UI::Core;
+using namespace Windows::UI::Popups;
+using namespace Windows::System;
+using namespace Windows::Foundation;
+using namespace Windows::Graphics::Display;
+using namespace Platform;
+
+
+// the class definition for the core "framework" of our app
+ref class App sealed : public IFrameworkView
+{
+private:
+	bool WindowClosed;
+public:
+	// some functions called by Windows
+	virtual void Initialize(CoreApplicationView^ AppView)
+	{
+		// set the OnActivated function to handle to Activated "event"
+		AppView->Activated += ref new TypedEventHandler
+			<CoreApplicationView^, IActivatedEventArgs^>(this, &App::OnActivated);
+
+		CoreApplication::Suspending += ref new EventHandler<SuspendingEventArgs^>(this, &App::Suspending);
+		CoreApplication::Resuming += ref new EventHandler<Object^>(this, &App::Resuming);
+
+		WindowClosed = false;
+	}
+
+	virtual void SetWindow(CoreWindow^ Window)
+	{
+		Window->Closed += ref new TypedEventHandler<CoreWindow^, CoreWindowEventArgs^>(this, &App::Closed);
+	}
+
+	virtual void Load(String^ EntryPoint) {}
+
+	virtual void Run()
+	{
+		//obtain a pointer to the window
+		CoreWindow ^window = CoreWindow::GetForCurrentThread();
+
+		while (!WindowClosed){
+			window->Dispatcher->ProcessEvents(CoreProcessEventsOption::ProcessAllIfPresent);
+
+			//game code here
+		}
+	}
+
+	virtual void Uninitialize() {}
+
+	// an "event" that is called when the application window is ready to be activated
+	void OnActivated(CoreApplicationView^ CoreAppView, IActivatedEventArgs^ Args)
+	{
+		CoreWindow ^Window = CoreWindow::GetForCurrentThread();
+		Window->Activate();
+	}
+
+	void Suspending(Object ^Sender, SuspendingEventArgs ^Args) {}
+	void Resuming(Object ^Sender, Object ^Args) {}
+
+	void Closed(CoreWindow ^sender, CoreWindowEventArgs ^args)
+	{
+		WindowClosed = true;
+	}
+};
+
+// the class definition that creates an instance of our core framework class
+ref class AppSource sealed : IFrameworkViewSource
+{
+public:
+	virtual IFrameworkView^ CreateView()
+	{
+		return ref new App();    // create an App class and return it
+	}
+};
+
+[MTAThread]    // define main() as a multi-threaded-apartment function
+
+// the starting point of all programs
+int main(Array<String^>^ args)
+{
+	CoreApplication::Run(ref new AppSource());    // create and run a new AppSource class
+	return 0;
+}
